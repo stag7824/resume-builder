d9956cc8ea2a22ea18f0489f134c5779a5761641
Syed Talha Ahmed Gardazi <atalha54@gmail.com>
2023-12-10 22:44:44 +0500

    update home for the resume

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container class="bg-grey-lighten-3" style="height: 100%; width: 100%; display: flex; justify-content: center; align-items: center; padding-top: 10%;">
+    <el-row :gutter="20">
+      <el-col :span="20" :offset="2" :xs="{ span: 24, offset: 0 }" :sm="{ span: 22, offset: 1 }" :md="{ span: 16, offset: 4 }" :lg="{ span: 12, offset: 6 }">
+        <el-card class="box-card" shadow="never" style="background-color: #f5f5f5;">
+          <div class="centered-text">
+            <h4>Craft Your Career Canvas – Build a Resume that Stands Out!</h4>
+          </div>
+          <div class="centered-text">
+            Transform your professional story with our easy-to-use resume builder. Tailor your experiences into a standout resume with our expert templates and tools. Step into the spotlight with a resume that reflects your unique skills and ambitions. Start crafting your career masterpiece now!
+          </div>
+          <br/>
+
+          <el-row class="centered-text" style="max-width: 300px; gap: 8px;">
+            <!-- Buttons Section -->
+            <el-button type="primary" size="large" block icon="el-icon-edit" @click="goToEditResume">
+              New Resume
+            </el-button>
+          </el-row>
+        </el-card>
+      </el-col>
+    </el-row>
+  </el-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<script>
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  methods: {
+    goToEditResume() {
+      this.$router.push({ name: 'edit.resume' });
+    },
+  },
 }
 </script>
+<style scoped>
+.centered-text {
+    text-align: center;
+    display: block;
+    margin: auto;
+}
+</style>
